9335f6d040e82220738ab51ac8dac3a337fcdc73
Brian Altman <bgaltman@gmail.com>
2023-04-17 14:34:41 +0200

    First commit

---
+++ public/js/chat.js
@@ -0,0 +1,115 @@
+const socket = io()
+
+// Elements of the form
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector("#send-location")
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage) // get the style properties from the browser (built-in browser function getComputedStyle())
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom) // get the marginBottom from the newest message
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin // add the margin to the elementheight
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight
+
+    // Height of the entire messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have I scrolled? Calculate this by getting the scroll distance from the top and adding the current height of the window
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) { // only autoscroll if the user is at the bottom, otherwise the user would be manually scrolling through the older messages
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
+socket.on('message', (message) => {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
+})
+
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        username: message.username,
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault() // this prevents the page from refreshing when the form is submitted
+    
+    $messageFormButton.setAttribute('disabled', 'disabled') // disable form
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+
+        $messageFormButton.removeAttribute('disabled') // enable form
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
+})
+
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!')
+            $sendLocationButton.removeAttribute('disabled')
+        })
+    })
+})
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
+})
